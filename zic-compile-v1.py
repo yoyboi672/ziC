13eb8e23fa00b666afff7b3128b3e3eb0bb932ac
yoyboi672 <truejelle672@icloud.com>
2024-10-18 19:23:00 +0200

    Make Compiler v1

---
+++ zic-compile-v1.py
@@ -0,0 +1,30 @@
+from time import sleep
+
+quit_when_error = True
+filename = input("Please enter a file name: ")
+
+try:
+    with open(f'{filename}.txt', 'r') as file:
+        code = file.read()
+except FileNotFoundError:
+    print(f"The input file '{filename}.txt' was not found.")
+    quit()
+
+if code == "":
+    print("The input file is empty.")
+    quit()
+
+for line in code.split('\n'):
+    if line.startswith('//') or line.strip() == "":
+        continue
+    
+    if line.startswith("say "):
+        print(line.removeprefix("say "))
+    elif line.startswith("delay "):
+        sleep(int(line.removeprefix("delay ").strip()))
+    else:
+        print("Invalid command:", line)
+        if quit_when_error:
+            quit()
+        else:
+            continue
